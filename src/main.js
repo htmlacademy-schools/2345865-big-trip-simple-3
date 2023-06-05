05953abe2bb81da8494565beed9061f9ceb7b357
Nick <signaalwespbij@yandex.ru>
2023-06-05 15:31:21 +0300

    big changes no.2 & fix gitignore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {render} from './framework/render.js';
-import {mockInit, tripPoints} from './mock/point.js';
-import {generateFilter} from './mock/filter.js';
-import {generateSorter} from './mock/sort.js';
+import {tripPoints} from './mock/point.js';
+import {mockInit} from './mock/utils.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TripPointModel from './model/trip-point-model.js';
@@ -10,11 +9,8 @@ mockInit(5, 10);
 
 const pageContainer = document.querySelector('.trip-events');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
-const filters = generateFilter();
-const sorters = generateSorter();
 const tripPointsModel = new TripPointModel(tripPoints);
-const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel, sorters});
+const boardPresenter = new BoardPresenter({boardContainer: pageContainer, tripPointsModel});
 
-render(new FilterView(filters), siteFilterElement);
+render(new FilterView(), siteFilterElement);
 boardPresenter.init();
-//already done
